68d691d18a5b57f89608c8cbf459bde4923bf3cb
irwinekabakti <irwinekabakti@gmail.com>
2024-05-11 01:55:05 +0700

    add:(setup redux)

--- app/OurTeam/page.tsx
+++ app/OurTeam/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import HeroOurTeam from "../(shared)/_HeroOurTeam/page";
+import axios from "axios";
 
 const OurTeam = () => {
+  // const getData = async () => {
+  //   try {
+  //     const { data } = await axios.get("https://randomuser.me/api/", {
+  //       params: {
+  //         results: 12,
+  //       },
+  //     });
+
+  //     console.log(data);
+  //   } catch (error) {
+  //     console.error("Error fetching data:", error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
   return (
     <main>
       <HeroOurTeam />
